0ed64ade94fab8c4caa17b702cbf3a5895b9e3bc
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-12-05 14:17:25 -0500

    make syndrome and gen_e public

--- mceliece6960119/encrypt.h
+++ mceliece6960119/encrypt.h
@@ -6,6 +6,7 @@
 #define ENCRYPT_H
 
 void encrypt(unsigned char *, const unsigned char *, unsigned char *);
+void gen_e(unsigned char *e);
+void syndrome(unsigned char *s, const unsigned char *pk, unsigned char *e);
 
 #endif
-
